e0919c811db2ebbd0f6eda86f58aed9d33141493
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2021-02-26 13:53:02 +0700

    Menambahkan Pemograman dengan Java

---
+++ Android RoadMap/Java/Pemograman_Java/src/Modul_0_Introduction/main.java
@@ -0,0 +1,7 @@
+package Modul_0_Introduction;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ Android RoadMap/Java/Pemograman_Java/src/Modul_1_Basic/modul_1_basic_string.java
@@ -0,0 +1,36 @@
+package Modul_1_Basic;
+
+public class modul_1_basic_string {
+    public static void main(String[] args) {
+
+        // Char to String
+        char[] dicodingChars = { 'd', 'i', 'c', 'o', 'd', 'i', 'n', 'g' };
+        String dicodingString = new String(dicodingChars);
+        System.out.println(dicodingString);
+
+        // String Length
+        int length = dicodingString.length();
+        System.out.println("Panjang string dari dicoding = "+length);
+
+        // ChartAt how to get char from string with index
+        for (int i = 0; i < dicodingString.length() ; i++) {
+            if (i == dicodingString.length()-1) System.out.println(dicodingString.charAt(i));
+            else System.out.print(dicodingString.charAt(i)+",");
+        }
+
+        // Contains apakah didalam string terdapat
+        Boolean cek = dicodingString.contains("di");
+        System.out.println("Apakah kata di terdapat pada variabel dicodingString : "+cek);
+
+        // Equals memeriksa 2 buah variabel memiliki nilai string yang sama
+        String dicodingString1 = "Dicoding";
+        String dicodingString2 = "dicoding";
+        System.out.println("string 0 dan 1 : "+dicodingString.equals(dicodingString1));
+        System.out.println("string 0 dan 2 : "+dicodingString.equals(dicodingString2));
+
+        // isEmpty apakah sebuah variabel memiliki string kosong atau tidak
+        String kosong = "";
+        System.out.println("Apakah Variabel kosong? => "+kosong.isEmpty());
+        System.out.println("Apakah Variabel dicodingString? => "+dicodingString.isEmpty());
+    }
+}
