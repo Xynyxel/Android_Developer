9d24a9d1d22bb77626fdf8c306d9181842471103
Tony Wijaya <tony18ti@mahasiswa.pcr.ac.id>
2021-02-26 14:33:52 +0700

    update

--- Android RoadMap/Java/Pemograman_Java/src/Modul_1_Basic/modul1_basic_OperasiEquality.java
+++ Android RoadMap/Java/Pemograman_Java/src/Modul_1_Basic/modul1_basic_OperasiEquality.java
@@ -1,4 +1,38 @@
 package Modul_1_Basic;
 
 public class modul1_basic_OperasiEquality {
+    public static void main(String[] args) {
+        int value = 5;
+        int anotherValue = 4;
+        boolean result;
+        result = value == anotherValue;
+
+        System.out.println("Hasil 'value == anotherValue' adalah " + result);
+        System.out.println();
+
+        System.out.println("Tidak sama dengan..");
+        result = value != anotherValue;
+        System.out.println("Hasil 'value != anotherValue' adalah " + result);
+        System.out.println();
+
+        System.out.println("Lebih besar dari..");
+        result = value > anotherValue;
+        System.out.println("Hasil 'value > anotherValue' adalah " + result);
+        System.out.println();
+
+        System.out.println("Sama atau lebih besar dari..");
+        result = value >= anotherValue;
+        System.out.println("Hasil 'value >= anotherValue' adalah " + result);
+        System.out.println();
+
+        System.out.println("Kurang dari..");
+        result = value < anotherValue;
+        System.out.println("Hasil 'value < anotherValue' adalah " + result);
+        System.out.println();
+        
+        System.out.println("Sama atau kurang dari dengan..");
+        result = value <= anotherValue;
+        System.out.println("Hasil 'result <= anotherValue' adalah " + result);
+        System.out.println();
+    }
 }
